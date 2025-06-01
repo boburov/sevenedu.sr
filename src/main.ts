613fa00015f8e4498f1affa721431fe4ed9718d3
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-06-01 17:46:49 +0500

    i'm trying to docker

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    }
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
-  
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
