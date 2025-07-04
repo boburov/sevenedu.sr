6dfd1aafb64096938548d34f2a6a0ae416d059c0
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-04 16:03:01 +0500

    fully complatyerd app ts file ffrom bakcend

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,24 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
 
- app.enableCors({});
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://sevenedu.uz',
+      'https://7-edu-admin-ehvf.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap().catch((err) => {
-  console.error(err);
-});
+bootstrap().catch(console.error);
