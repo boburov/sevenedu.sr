0f7af0878db49e7a904d302d8a32fe41730784b2
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-19 17:20:58 +0500

    all comits push

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://sevenedu.store'],
+    origin: [],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   });
