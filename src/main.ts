7591ed3f14db3d919367328977a26ce83c18634b
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-19 18:54:37 +0500

    pushhh

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { rawBody: true, bodyParser: false });
-
-
-  app.use(bodyParser.json({ limit: '4000mb' }));
-  app.use(bodyParser.urlencoded({ limit: '4000mb', extended: true }));
-
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      "*"
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://sevenedu.uz',
+        'https://7-edu-admin-ehvf.vercel.app'
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
-
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-
   const server = await app.listen(process.env.PORT ?? 3000);
-  server.setTimeout(1000 * 60 * 50);
+  server.setTimeout(1000 * 60 * 50); // 50 minutes
 }
 bootstrap();
