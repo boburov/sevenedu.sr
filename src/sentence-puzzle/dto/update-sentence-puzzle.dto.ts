a19ce0bd95490e7563750fbcf93dfc99e55daff8
Boburov Shukurullo <boburovdev@gmail.com>
2025-09-14 02:48:24 +0500

    fixed

--- src/sentence-puzzle/dto/update-sentence-puzzle.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSentencePuzzleDto } from './create-sentence-puzzle.dto';
-
-export class UpdateSentencePuzzleDto extends PartialType(CreateSentencePuzzleDto) {}
--- src/sentence-puzzle/sentence-puzzle.module.ts
+++ src/sentence-puzzle/sentence-puzzle.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SentencePuzzleService } from './sentence-puzzle.service';
 import { SentencePuzzleController } from './sentence-puzzle.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+   imports: [PrismaModule], 
   controllers: [SentencePuzzleController],
   providers: [SentencePuzzleService],
 })
